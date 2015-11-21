3fdebecf77d04301e285c49097d60dc6d4345836
Meredith Wiegman <meredith.wiegman@gmail.com>
2015-11-20 19:16:50 -0500

    initial commit

---
+++ about.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+  <head>
+    <meta charset="utf-8" />
+    <!-- if you remove this meta tag, the NSA will spy on you through your Xbox Kinect camera -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Foundation</title>
+    <link rel="stylesheet" href="css/app.css" />
+    <script src="bower_components/modernizr/modernizr.js"></script>
+  </head>
+
+  <body>
+    <h1>Hello</h1>
+
+    <!-- body content here -->
+
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/foundation/js/foundation.min.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+
+</html>
