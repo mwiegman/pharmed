636e9bb48908839cd41a684da31a3bcd49b10f76
Meredith Wiegman <meredith.wiegman@gmail.com>
2016-01-12 12:40:44 -0500

    jekyll build

---
+++ _site/js/app.js
@@ -0,0 +1,11 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation({
+  offcanvas : {    
+    close_on_click : true
+  }
+});
+
+$(function() {
+        $('body').removeClass('fade-out');    
+      });
